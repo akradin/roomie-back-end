da28fbf155421fd58275987f60224bc192c82492
akradin <alex.kradin@gmail.com>
2016-12-01 14:31:38 -0500

    Add validation Steps
    Date must correct format
    Name must be present
    difficulty length can only be one

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,2 +1,5 @@
 class Chore < ActiveRecord::Base
+  validates :name, presence: true
+  validates :due_date, format: { with: /\d{4}\-\d{2}\-\d{2}/ }
+  validates :difficulty, length: { maximum: 1 }
 end
